6e921b3e06abdaa183b7abc39f30908079bd815d
Anton Kechashin <atk@sitecore.net>
2017-08-17 23:04:10 +0300

    Initial commit

---
+++ src/Sitecore.Support.179355/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.179355")]
+[assembly: AssemblyProduct("Sitecore.Support.179355")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.179355/Workflows/Simple/WorkflowProvider.cs
@@ -0,0 +1,31 @@
+﻿namespace Sitecore.Support.Workflows.Simple
+{
+    using Sitecore.Data.Items;
+    using Sitecore.Workflows;
+
+    public class WorkflowProvider : Sitecore.Workflows.Simple.WorkflowProvider
+    {
+        public WorkflowProvider(string databaseName, HistoryStore historyStore) : base(databaseName, historyStore)
+        {
+        }
+
+        public override IWorkflow[] GetWorkflows()
+        {
+            Item item = this.Database.Items[ItemIDs.WorkflowRoot];
+            if (item == null)
+            {
+                return new IWorkflow[0];
+            }
+            #region Modified code
+            // Item[] itemArray = item.Children.ToArray();
+            Item[] itemArray = item.Axes.SelectItems("descendant::*[@@templateid='" + Sitecore.TemplateIDs.Workflow.ToString() + "']");
+            #endregion
+            IWorkflow[] workflowArray = new IWorkflow[itemArray.Length];
+            for (int i = 0; i < itemArray.Length; i++)
+            {
+                workflowArray[i] = this.InstantiateWorkflow(itemArray[i].ID.ToString(), this);
+            }
+            return workflowArray;
+        }
+    }
+}
